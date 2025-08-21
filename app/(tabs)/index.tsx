8fbc28fea095f46ba5fdc6ca2c160103a11a755e
faf <knachinen@gmail.com>
2025-08-21 20:42:09 +0900

    add: trending

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,8 +1,11 @@
 import MovieCard from "@/components/MovieCard";
 import SearchBar from "@/components/SearchBar";
+import TrendingCard from "@/components/TrendingCard";
+
 import { icons } from "@/constants/icons";
 import { images } from "@/constants/images";
 import { fetchMovies } from "@/services/api";
+import { getTrendingMovies } from "@/services/appwrite";
 import useFetch from "@/services/usefetch";
 import { useRouter } from "expo-router";
 import {
@@ -16,6 +19,12 @@ import {
 
 export default function Index() {
   const router = useRouter();
+
+  const {
+    data: trendingMovies,
+    loading: trendingLoading,
+    error: trendingError,
+  } = useFetch(getTrendingMovies);
 
   const {
     data: movies,
@@ -38,20 +47,45 @@ export default function Index() {
         >
           <Image source={icons.logo} className="w-12 h-10 mt-20 mb-5 mx-auto" />
 
-          {moviesLoading ? (
+          {moviesLoading || trendingLoading ? (
             <ActivityIndicator
               size="large"
               color="#0000ff"
               className="mt-10 self-center"
             />
-          ) : moviesError ? (
-            <Text className="text-red-500">Error: {moviesError?.message}</Text>
+          ) : moviesError || trendingError ? (
+            <Text className="text-red-500">
+              Error: {moviesError?.message || trendingError?.message}
+            </Text>
           ) : (
             <View className="flex-1 mt-5">
               <SearchBar
                 onPress={() => router.push("/search")}
                 placeholder="Search for movies, series, and more"
               />
+
+              {trendingMovies && (
+                <View className="mt-10">
+                  <Text className="text-lg text-white font-bold mb-3">
+                    Trending Movies
+                  </Text>
+                  <FlatList
+                    horizontal
+                    showsHorizontalScrollIndicator={false}
+                    className="mb-4 mt-3"
+                    data={trendingMovies}
+                    contentContainerStyle={{
+                      gap: 26,
+                    }}
+                    renderItem={({ item, index }) => (
+                      <TrendingCard movie={item} index={index} />
+                    )}
+                    keyExtractor={(item) => item.movie_id.toString()}
+                    ItemSeparatorComponent={() => <View className="w-4" />}
+                  />
+                </View>
+              )}
+
               <>
                 <Text className="text-lg text-white font-bold mt-5 mb-3">
                   Latest Movies
--- app/(tabs)/search.tsx
+++ app/(tabs)/search.tsx
@@ -3,6 +3,7 @@ import SearchBar from "@/components/SearchBar";
 import { icons } from "@/constants/icons";
 import { images } from "@/constants/images";
 import { fetchMovies } from "@/services/api";
+import { updateSearchCount } from "@/services/appwrite";
 import useFetch from "@/services/usefetch";
 import { useEffect, useState } from "react";
 import { ActivityIndicator, FlatList, Image, Text, View } from "react-native";
@@ -23,11 +24,6 @@ const Search = () => {
     const timeoutId = setTimeout(async () => {
       if (searchQuery.trim()) {
         await loadMovies();
-
-        // Call updateSearchCount only if there are results
-        // if (movies?.length! > 0 && movies?.[0]) {
-        //   await updateSearchCount(searchQuery, movies[0]);
-        // }
       } else {
         reset();
       }
@@ -35,6 +31,14 @@ const Search = () => {
 
     return () => clearTimeout(timeoutId);
   }, [searchQuery]);
+
+  // Update search count in Appwrite
+  useEffect(() => {
+    // Call updateSearchCount only if there are results
+    if (movies?.length! > 0 && movies?.[0]) {
+      updateSearchCount(searchQuery, movies[0]);
+    }
+  }, [movies]);
 
   const handleSearch = (text: string) => {
     setSearchQuery(text);
