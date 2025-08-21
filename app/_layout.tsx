67d1b4896a5d795bc4eb0d6dc62386bcf10104ac
faf <knachinen@gmail.com>
2025-08-21 20:55:48 +0900

    mod: hide. statusbar

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,17 +1,21 @@
 import { Stack } from "expo-router";
+import { StatusBar } from "expo-status-bar";
 import "./globals.css"; // Import global styles
 
 export default function RootLayout() {
   return (
-    <Stack>
-      <Stack.Screen
-        name="(tabs)"
-        options={{ headerShown: false }} // Hide header for the main screen;
-      />
-      <Stack.Screen
-        name="movies/[id]"
-        options={{ headerShown: false }} // Hide header for the onboarding screen
-      />
-    </Stack>
+    <>
+      <StatusBar hidden={true} />
+      <Stack>
+        <Stack.Screen
+          name="(tabs)"
+          options={{ headerShown: false }} // Hide header for the main screen;
+        />
+        <Stack.Screen
+          name="movies/[id]"
+          options={{ headerShown: false }} // Hide header for the onboarding screen
+        />
+      </Stack>
+    </>
   );
 }
