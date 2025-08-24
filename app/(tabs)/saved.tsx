2701ed90f4e9d672b0ac5d5a702642ad569d601d
faf <knachinen@gmail.com>
2025-08-24 17:50:52 +0900

    mod: saved card style

--- app/(tabs)/saved.tsx
+++ app/(tabs)/saved.tsx
@@ -1,8 +1,7 @@
 import SavedCard from "@/components/SavedCard";
-import { icons } from "@/constants/icons";
 import { getSavedMovies } from "@/services/appwrite";
 import useFetch from "@/services/usefetch";
-import { ActivityIndicator, FlatList, Image, Text, View } from "react-native";
+import { ActivityIndicator, FlatList, Text, View } from "react-native";
 
 const Saved = () => {
   const {
@@ -12,11 +11,11 @@ const Saved = () => {
   } = useFetch(getSavedMovies);
 
   return (
-    <View className="flex-1 bg-primary px-10">
-      <View className="flex flex-1 flex-col items-center justify-center gap-5">
+    <View className="flex-1 bg-primary px-5">
+      {/* <View className="flex flex-1 flex-col items-center justify-center gap-5">
         <Image source={icons.save} className="size-10" tintColor={"#FFF"} />
         <Text className="text-gray-500 text-base">Saved</Text>
-      </View>
+      </View> */}
       {savedLoading ? (
         // Loading state
         <ActivityIndicator
@@ -29,22 +28,28 @@ const Saved = () => {
         <Text className="text-red-500">Error: {savedError?.message}</Text>
       ) : (
         // Data loaded state
-        <View className="flex-1 mt-5">
-          <FlatList
-            data={savedMovies}
-            renderItem={({ item }) => <SavedCard {...item} />}
-            keyExtractor={(item) => item.movie_id.toString()}
-            numColumns={3}
-            columnWrapperStyle={{
-              justifyContent: "flex-start",
-              gap: 20,
-              paddingRight: 5,
-              marginBottom: 10,
-            }}
-            className="mt-2 pb-32"
-            scrollEnabled={false}
-          />
-        </View>
+        <>
+          <View className="flex-1 mt-5">
+            <Text className="text-lg text-white font-bold mt-5 mb-3">
+              Saved Movies
+            </Text>
+            <FlatList
+              data={savedMovies}
+              renderItem={({ item }) => <SavedCard {...item} />}
+              keyExtractor={(item) => item.movie_id.toString()}
+              numColumns={3}
+              columnWrapperStyle={{
+                justifyContent: "flex-start",
+                gap: 20,
+                paddingRight: 5,
+                marginBottom: 10,
+              }}
+              className="mt-2 pb-32"
+              // scrollEnabled={true}
+              showsVerticalScrollIndicator={false}
+            />
+          </View>
+        </>
       )}
     </View>
   );
